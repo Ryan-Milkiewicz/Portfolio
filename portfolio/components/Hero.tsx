7e3cf49e085267f9f8a2a2403aea3303bfcd5f42
rmilkiewicz <rmilkiewicz@Hoffman-Development.com>
2022-12-22 10:29:51 -0500

    experience section

--- portfolio/components/Hero.tsx
+++ portfolio/components/Hero.tsx
@@ -50,5 +50,5 @@ export default function Hero({}: Props) {
             </div>
         </div>
     </div>
-  )
+  );
 }
--- portfolio/pages/index.tsx
+++ portfolio/pages/index.tsx
@@ -3,6 +3,7 @@ import Head from 'next/head'
 import Header from '../components/Header';
 import Hero from '../components/Hero';
 import About from '../components/About';
+import WorkExperience from "../components/WorkExperience";
 
 const Home: NextPage = () => {
   //left off at 1:25
@@ -23,6 +24,11 @@ const Home: NextPage = () => {
       {/* About */}
       <section id="about" className="snap-center">
         <About />
+      </section>
+
+         {/* Experience */}
+         <section id="experience" className="snap-center">
+        < WorkExperience/>
       </section>
    
     </div>
