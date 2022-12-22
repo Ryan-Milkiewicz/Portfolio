96db7e66fd009cb4c9d425bf247106752a774c3b
RyanMilkiewicz <ryanmilkiewicz@gmail.com>
2022-12-21 23:16:12 -0500

    about section

--- portfolio/pages/index.tsx
+++ portfolio/pages/index.tsx
@@ -2,8 +2,10 @@ import type { NextPage } from "next";
 import Head from 'next/head'
 import Header from '../components/Header';
 import Hero from '../components/Hero';
+import About from '../components/About';
 
 const Home: NextPage = () => {
+  //left off at 1:25
   return (
     <div className="bg-[rgb(36,36,36)] text-white h-screen snap-y 
       snap-mandatory overflow-scroll z-0">
@@ -13,9 +15,14 @@ const Home: NextPage = () => {
 
       <Header />
 
-      {/*Hero*/}
+      {/* Hero */}
       <section id="hero" className="snap-center">
         <Hero />
+      </section>
+
+      {/* About */}
+      <section id="about" className="snap-center">
+        <About />
       </section>
    
     </div>
