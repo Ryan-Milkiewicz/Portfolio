9d9f26c389661f48c3d8482485a60aaa3bd09008
RyanMilkiewicz <ryanmilkiewicz@gmail.com>
2022-12-23 10:40:11 -0500

    contract form email

--- portfolio/pages/index.tsx
+++ portfolio/pages/index.tsx
@@ -12,7 +12,8 @@ const Home: NextPage = () => {
   //left off at 2:20
   return (
     <div className="bg-[rgb(36,36,36)] text-white h-screen snap-y 
-      snap-mandatory overflow-scroll z-0">
+      snap-mandatory overflow-y-scroll overflow-x-hidden z-0
+      scrollbar scrollbar-track-gray-400/20 scrollbar-thumb-[#F7AB0A]/80">
       <Head>
         <title>Ryan's Portfolio</title>
       </Head>
