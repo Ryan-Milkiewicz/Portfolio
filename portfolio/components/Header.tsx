0ed76982b5b31d17b06740b3787c57b64be5ea2b
RyanMilkiewicz <ryanmilkiewicz@gmail.com>
2022-12-23 11:06:31 -0500

    added link tag to get in touch logo

--- portfolio/components/Header.tsx
+++ portfolio/components/Header.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { SocialIcon } from 'react-social-icons';
 import { motion } from 'framer-motion';
+import Link from 'next/link';
 
 
 type Props = {}
@@ -49,10 +50,13 @@ function Header({}: Props) {
                 network="email"
                 fgColor="gray"
                 bgColor="transparent"
+                url='#contact'
             />
-            <p className="uppercase hidden md:inline-flex text-sm text-gray-400">
-                Get In Touch
-            </p>
+            <Link href="#contact">
+                <p className="uppercase hidden md:inline-flex text-sm text-gray-400">
+                    Get In Touch
+                </p>
+            </Link>
         </motion.div>
     </header>
   )
