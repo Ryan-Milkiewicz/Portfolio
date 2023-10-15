e610a4637b85b58335b05aa0dda733ff706e43eb
RyanMilkiewicz <ryanmilkiewicz@gmail.com>
2023-10-14 21:05:35 -0400

    added project schemas to sanity

--- sanity/schemas/index.ts
+++ sanity/schemas/index.ts
@@ -2,10 +2,11 @@ import pageInfo from './pageInfo';
 import experience from './experience';
 import skill from './skill';
 import social from './social';
+import project from './project';
 import { createSchema } from 'sanity';
 
 
-export const schemaTypes = [pageInfo, experience, skill, social];
+export const schemaTypes = [pageInfo, experience, skill, project, social];
 
 
 export default createSchema({
