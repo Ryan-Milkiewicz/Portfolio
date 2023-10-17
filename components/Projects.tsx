a9eb3398958268dd81e4ef8336d86c808d5058a3
RyanMilkiewicz <ryanmilkiewicz@gmail.com>
2023-10-17 00:22:37 -0400

    code clean

--- components/Projects.tsx
+++ components/Projects.tsx
@@ -24,9 +24,9 @@ function Projects({ projects }: Props) {
         <div className="relative w-full flex overflow-x-scroll overflow-y-hidden snap-x snap-mandatory z-20 
         scrollbar-thin scrollbar-track-gray-400/20 scrollbar-thumb-[#F7AB0A]/80" >
           {projects?.map((project, i) => (
-            // eslint-disable-next-line react/jsx-key
+        
             <div className="w-screen flex-shrink-0 snap-center flex flex-col space-y-5 items-center justify-center
-            p-20 md:p-44 h-screen">
+            p-20 md:p-44 h-screen" key={i}>
               <motion.img 
                 initial={{
                   y: -300,
@@ -41,9 +41,6 @@ function Projects({ projects }: Props) {
 
                 <div className="space-y-10 px-0 md:px-10 max-w-6xl">
                   <h4 className="text-4xl font-semibold text-center">
-                    <span className="underline decoration-[#F7AB0A]/50">
-                      Case Study {i + 1} of {project.length}:
-                    </span>{" "}
                     {project?.title}
                   </h4>
 
