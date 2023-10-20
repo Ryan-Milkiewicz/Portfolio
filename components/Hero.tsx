582569943330d2d8f9711d77025ece545fe441c9
RyanMilkiewicz <ryanmilkiewicz@gmail.com>
2023-10-19 23:38:31 -0400

    remove images and pull from sanity instead

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -2,12 +2,15 @@ import React from 'react';
 import { Cursor, useTypewriter } from 'react-simple-typewriter';
 import BackgroundCircles from './BackgroundCircles';
 import Image from 'next/image';
-import profileImg from '../public/profile-image.jpeg';
 import Link from 'next/link';
+import { PageInfo } from '../typings';
+import { urlFor } from '../sanity';
 
-type Props = {}
+type Props = {
+    pageInfo: PageInfo;
+}
 
-export default function Hero({}: Props) {
+export default function Hero({pageInfo}: Props) {
     const [text, count] = useTypewriter({
         words: [
             "Hi, The Name's Ryan Milkiewicz",
@@ -22,13 +25,16 @@ export default function Hero({}: Props) {
     <div className="h-screen flex flex-col space-y-8 items-center justify-center text-center overflow-hidden">
         <BackgroundCircles />
         <Image
+            unoptimized
             className="relative rounded-fill h-32 w-32 mx-auto"
-            src={profileImg}
+            src={urlFor(pageInfo?.heroImage).url()}
             alt="profile-image" 
+            height={32}
+            width={32}
         />
         <div className="z-20">
             <h2 className="text-sm uppercase text-gray-500 pb-2 tracking-[15px]">
-                Software Developer
+                {pageInfo?.role}
             </h2>
             <h1 className="text-5xl lg:text-6xl font-semibold px-10">
                 <span className="mr-3">{text}</span>
