9983aec790d05d59af61f27bf46d0379d7b0fa03
RyanMilkiewicz <ryanmilkiewicz@gmail.com>
2025-01-03 12:03:24 -0500

    fixes to get project to deploy to vercel

--- sanity/sanity.config.ts
+++ sanity/sanity.config.ts
@@ -1,16 +1,14 @@
 import {defineConfig} from 'sanity'
 import {deskTool} from 'sanity/desk'
-import {visionTool} from '@sanity/vision'
+//import {visionTool} from '@sanity/vision'
 import {schemaTypes} from './schemas'
 
 export default defineConfig({
   name: 'default',
   title: 'personal-portfolio',
-
   projectId: 'fmswh3nj',
   dataset: 'production',
-
-  plugins: [deskTool(), visionTool()],
+  plugins: [deskTool()],
 
   schema: {
     types: schemaTypes,
